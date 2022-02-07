b8e82151ad08a369e0d8027be7ed011bea61681c
4p0ca1yp53 <12derciomaduna@gmail.com>
2022-02-07 14:08:24 +0200

    Completed the template skeloton

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Nav from "../components/Nav"
+import MyPhotos from "../components/MyPhotos"
+import Box from "@mui/material/Box";
 
 export default function Home() {
   return (
-    <main style={{
+    <Box style={{
       display:'flex'
     }}>
      <Nav />
-     <section style={{flex:10,height:'100vh'}}>
-
-     </section>
-    </main>
+     <Box sx={{flex:10,minHeight:'100vh'}}>
+      <MyPhotos />
+     </Box>
+    </Box>
   )
 }
