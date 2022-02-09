4c4350a6d9e7c284d889ed3b1a7068f804737e00
scahdrack <schadrackbotombe@gmail.com>
2022-02-09 13:19:19 +0200

    completed about-section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Nav from "../components/Nav";
-import MyPhotos from "../components/MyPhotos";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import HomePage from "../components/HomePage";
